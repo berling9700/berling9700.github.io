726e6a7daadc4d73f0bde454c43044037dd655e3
rberling <berling.19@buckeyemail.osu.edu>
2024-02-17 13:45:38 -0500

    updates

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
+  // Get the button element
+var pdfButton = document.getElementById('pdf-button');
+
+// Add click event listener to the button
+pdfButton.addEventListener('click', function() {
+    window.open('pdfs/Robby Berling Resume.pdf', '_blank');
+});
