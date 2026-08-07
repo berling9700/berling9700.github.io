10b57188716b37ea5d65de95d3cdca8d7869ed2e
rberling <berling.19@buckeyemail.osu.edu>
2026-08-06 22:57:06 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isProduction = mode === 'production';
+
     return {
+      base: isProduction ? './' : '/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
